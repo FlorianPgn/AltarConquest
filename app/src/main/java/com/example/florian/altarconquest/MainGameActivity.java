161b8c4d7f2cec8383df48ce4231166d9cc6fab6
Florian Pgn <florianpgn@gmail.com>
2016-10-13 23:18:54 +0200

    Caméra de départ sur le Dép
    La caméra de départ est sur le département avec un zoom approprié

--- app/src/main/java/com/example/florian/altarconquest/MainGameActivity.java
+++ app/src/main/java/com/example/florian/altarconquest/MainGameActivity.java
@@ -39,8 +39,8 @@ public class MainGameActivity extends FragmentActivity implements OnMapReadyCall
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(48.08604927627401, -0.7595989108085632);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18.0f));
     }
 }
