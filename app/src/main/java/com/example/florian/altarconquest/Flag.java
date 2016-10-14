4214eac1e7977d948e843fdcd483721c35b47c90
Florian Pgn <florianpgn@gmail.com>
2016-10-14 11:34:45 +0200

    Précision de la class Flag

--- app/src/main/java/com/example/florian/altarconquest/Flag.java
+++ app/src/main/java/com/example/florian/altarconquest/Flag.java
@@ -9,8 +9,28 @@ import com.google.android.gms.maps.model.LatLng;
 public class Flag {
     private String name;
     private LatLng coordonees;
+    private boolean capturable = false;
+    private TeamColor teamColor;
 
-    public Flag(String name, LatLng coordonees){
+    public Flag(String name, LatLng coordonees, TeamColor teamColor){
+        this.name = name;
         this.coordonees = coordonees;
+        this.teamColor = teamColor;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public LatLng getCoordonees(){
+        return coordonees;
+    }
+
+    public boolean isCapturable(){
+        return capturable;
+    }
+
+    public TeamColor getTeamColor(){
+        return teamColor;
     }
 }
