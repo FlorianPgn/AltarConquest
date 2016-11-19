4f10e3c14357c3ec8405f5c2f73a43b4620fa92c
FlorianPgn <florianpgn@gmail.com>
2016-11-19 13:29:19 +0100

    Ajouter Permission localisation + affichage position joueur

--- app/src/main/java/com/example/florian/altarconquest/View/EcranJeu.java
+++ app/src/main/java/com/example/florian/altarconquest/View/EcranJeu.java
@@ -1,8 +1,12 @@
 package com.example.florian.altarconquest.View;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -18,8 +22,6 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
@@ -29,6 +31,8 @@ public class EcranJeu extends FragmentActivity implements OnMapReadyCallback {
     private Button mapButton, flagButton, qrCodeButton, treeButton, unactiveTreeButton;
     private RelativeLayout ecran;
     private ArrayList<Button> boutonsDeployables;
+
+    private final int REQUEST_CODE = 128;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -156,8 +160,40 @@ public class EcranJeu extends FragmentActivity implements OnMapReadyCallback {
         groundOverlayOptions.image(image).position(new LatLng(48.10872932860948, -0.7233687971115112), 380f).transparency(0.2f);
         GroundOverlay imageOverlay = mMap.addGroundOverlay(groundOverlayOptions);
 
+
+        demanderPermissionGps();
+
+
         Game game = new Game("test", this);
         game.launchServerRequest();
 
+    }
+
+    public void demanderPermissionGps(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        mMap.setMyLocationEnabled(true);
+                    }
+
+                } else {
+                    demanderPermissionGps();
+                }
+                return;
+            }
+
+        }
     }
 }
