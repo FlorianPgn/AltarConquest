37362e92c4c9dc30d1f763b4ec3ea93ee9254e3c
Pyraxow <fouraces@live.fr>
2016-12-12 01:15:05 +0100

    Ajout choix equipe + couleur joueurs

--- app/src/main/java/com/example/florian/altarconquest/Controller/ChoixEquipeListener.java
+++ app/src/main/java/com/example/florian/altarconquest/Controller/ChoixEquipeListener.java
@@ -9,15 +9,15 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.example.florian.altarconquest.Model.TeamColor;
+import com.example.florian.altarconquest.ServerInteractions.ServerSendPlayerColor;
 import com.example.florian.altarconquest.ServerInteractions.ServerSendPlayerProperties;
-import com.example.florian.altarconquest.ServerInteractions.ServerSendTeamColorProperties;
 import com.example.florian.altarconquest.View.EcranLobby_Partie;
 
 /**
  * Created by Hugo on 11/12/2016.
  */
 
-public class ChoixEquipeListener implements View.OnClickListener {
+public class ChoixEquipeListener {
 
     private String player;
     private String color;
@@ -25,14 +25,9 @@ public class ChoixEquipeListener implements View.OnClickListener {
     public ChoixEquipeListener(String player, String color){
         this.player = player;
         this.color = color;
-    }
 
-    @Override
-    public void onClick(View v)
-    {
-        if(player != "" && color != "")
-        {
-            ServerSendTeamColorProperties ssp = new ServerSendTeamColorProperties();
+        if(player != "" && color != "") {
+            ServerSendPlayerColor ssp = new ServerSendPlayerColor();
             ssp.execute(player, color);
         }
     }
