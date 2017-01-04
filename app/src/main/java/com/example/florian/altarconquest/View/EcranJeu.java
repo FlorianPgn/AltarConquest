b3f2232da2e97114bacbf4aed5cbfa91f7e92456
FlorianPgn <florianpgn@gmail.com>
2017-01-04 21:44:45 +0100

    Added security on permissions request and some enhancement

--- app/src/main/java/com/example/florian/altarconquest/View/EcranJeu.java
+++ app/src/main/java/com/example/florian/altarconquest/View/EcranJeu.java
@@ -3,10 +3,10 @@ package com.example.florian.altarconquest.View;
 import com.example.florian.altarconquest.Model.Player;
 import com.example.florian.altarconquest.Model.TeamColor;
 import com.example.florian.altarconquest.R;
+
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -26,7 +26,9 @@ import com.example.florian.altarconquest.Model.Game;
 import com.example.florian.altarconquest.ServerInteractions.ServerReceptionCoordinates;
 import com.example.florian.altarconquest.ServerInteractions.ServerReceptionFlagsPositions;
 import com.example.florian.altarconquest.ServerInteractions.ServerSendCoordinates;
+
 import android.location.LocationListener;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -79,16 +81,21 @@ public class EcranJeu extends FragmentActivity implements OnMapReadyCallback, Lo
         mapFragment.getMapAsync(this);
 
         imageEconomie = (ImageView) findViewById(R.id.economyEnergie);
+
+        coordinates = new HashMap<>();
+
         economieEnergie = new EcomieEnergie(this);
         economieEnergie.start();
 
+        //Récupère l'objet Game du lobby
         Bundle bundle = getIntent().getExtras();
         game = bundle.getParcelable("game");
 
+        //Récupère et sécurise les données passées depuis le lobby
         String color;
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
-            if(extras == null) {
+            if (extras == null) {
                 pseudo = null;
                 color = null;
             } else {
@@ -100,32 +107,97 @@ public class EcranJeu extends FragmentActivity implements OnMapReadyCallback, Lo
             color = (String) savedInstanceState.getSerializable("STRING_COLOR");
         }
 
+        //Initialise la team du joueur pendant cette partie
         if (color.equals(TeamColor.BLUE.toString())) {
             myTeamColor = TeamColor.BLUE;
         } else if (color.equals(TeamColor.RED.toString())) {
             myTeamColor = TeamColor.RED;
         }
-
-        coordinates = new HashMap<>();
-
-        initialisationBoutons();
     }
 
-    public void afficherJoueurs(){
-        for(Player player : game.getTeam(myTeamColor).getListeDesPlayers()) {
-            if(!player.getPseudo().equals(pseudo))
-                updateCircles(player);
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        double depInfoLat = 48.08574927627401, depInfoLng = -0.7584989108085632;
+        double echologiaLat = 48.10922932860948, echologiaLng = -0.7235687971115112;
+
+        // Initialisation de la position de départ de la caméra
+        LatLng startCameraPosition = new LatLng(depInfoLat, depInfoLng);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(startCameraPosition, 17.0f));
+        retirerMouvementCameraMarkers();
+
+        GroundOverlayOptions groundOverlayOptions = new GroundOverlayOptions();
+        BitmapDescriptor image = BitmapDescriptorFactory.fromResource(R.drawable.echologia_map);
+
+        groundOverlayOptions.image(image).position(new LatLng(48.10872932860948, -0.7233687971115112), 380f).transparency(0.2f);
+        GroundOverlay imageOverlay = mMap.addGroundOverlay(groundOverlayOptions);
+
+        creationMenuDeroulant();
+        demanderPermissionGps();
+        recupererLesDrapeauxSurLeServeur(game);
+
+        //Timer qui fait toutes les requêtes à faire durant la partie
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                ServerReceptionCoordinates src = new ServerReceptionCoordinates(game);
+                src.execute(String.valueOf(game.getId()));
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        afficherJoueurs();
+                    }
+                });
+                ServerSendCoordinates ssc = new ServerSendCoordinates();
+                ssc.execute(pseudo, String.valueOf(game.getId()), String.valueOf(location.getLatitude()), String.valueOf(location.getLongitude()));
+            }
+        };
+        timer.schedule(timerTask, 0, 1000 * 2);
+    }
+    
+
+    //Méthodes pour afficher les drapeaux au démarage de l'activité
+    public void afficherDrapeaux(Game game) {
+        for (Flag flag : game.getBlueTeam().getListofFlags()) {
+            mMap.addMarker(new MarkerOptions().position(flag.getCoordonnees()).title(flag.getName()));
+        }
+        for (Flag flag : game.getRedTeam().getListofFlags()) {
+            mMap.addMarker(new MarkerOptions().position(flag.getCoordonnees()).title(flag.getName()));
         }
     }
 
-    public void updateCircles(Player player){
+    public void recupererLesDrapeauxSurLeServeur(Game game) {
+        Log.i("Début", "requete serveur flags");
+        ServerReceptionFlagsPositions srf = new ServerReceptionFlagsPositions(game, this);
+        srf.execute();
+    }
+
+
+    //Méthodes pour afficher la position des joueurs dont on est censé avoir la position
+    public void afficherJoueurs() {
+        for (Player player : game.getTeam(myTeamColor).getListeDesPlayers()) {
+            if (!player.getPseudo().equals(pseudo))
+                updateAffichagePositionJoueurs(player);
+        }
+    }
+
+    public void updateAffichagePositionJoueurs(Player player) {
         Circle circle = coordinates.get(player.getPseudo());
         if (circle == null) {
             // Instantiates a new CircleOptions object and defines the center and radius
             CircleOptions circleOptions = new CircleOptions()
                     .center(player.getCoordonnees())
                     .radius(2); // In meters
-
             circle = mMap.addCircle(circleOptions);
             coordinates.put(player.getPseudo(), circle);
         } else {
@@ -133,6 +205,32 @@ public class EcranJeu extends FragmentActivity implements OnMapReadyCallback, Lo
         }
     }
 
+    //Retire le mouvement de caméra quand on clic sur un Marker
+    public void retirerMouvementCameraMarkers(){
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            Marker currentShown;
+            public boolean onMarkerClick(Marker marker) {
+                if (marker.equals(currentShown)) {
+                    marker.hideInfoWindow();
+                    currentShown = null;
+                } else {
+                    marker.showInfoWindow();
+                    currentShown = marker;
+                }
+                return true;
+            }
+        });
+    }
+
+    //Met à jour les coordonnées du joueur si elles ont changées
+    @Override
+    public void onLocationChanged(Location location) {
+        this.location = location;
+        Log.i("Location", location.getLatitude()+" "+location.getLongitude());
+    }
+
+
+    //Methodes pour le menu déployable en bas de l'écran
     public void onWindowFocusChanged(boolean hasFocus) {
 
         super.onWindowFocusChanged(hasFocus);
@@ -153,11 +251,9 @@ public class EcranJeu extends FragmentActivity implements OnMapReadyCallback, Lo
                 }
             }
         });
-
-
     }
 
-    public void initialisationBoutons(){
+    public void creationMenuDeroulant(){
         boutonsDeployables = new ArrayList<Button>();
 
         attackToken = (ImageView) findViewById(R.id.attackToken);
@@ -219,143 +315,57 @@ public class EcranJeu extends FragmentActivity implements OnMapReadyCallback, Lo
         startActivity(intent);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        double depInfoLat = 48.08574927627401;
-        double depInfoLng = -0.7584989108085632;
-
-        double echologiaLat = 48.10922932860948;
-        double echologiaLng = -0.7235687971115112;
-
-
-
-        // Initialisation de la position de départ de la caméra
-        LatLng startCameraPosition = new LatLng(depInfoLat, depInfoLng);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(startCameraPosition, 17.0f));
-        retirerMouvementCameraMarkers();
-
-        GroundOverlayOptions groundOverlayOptions = new GroundOverlayOptions();
-        BitmapDescriptor image = BitmapDescriptorFactory.fromResource(R.drawable.echologia_map);
-
-        groundOverlayOptions.image(image).position(new LatLng(48.10872932860948, -0.7233687971115112), 380f).transparency(0.2f);
-        GroundOverlay imageOverlay = mMap.addGroundOverlay(groundOverlayOptions);
-
-
-
-        demanderPermissionGps();
-        launchServerRequest(game);
-
-        Timer timer = new Timer();
-        TimerTask timerTask = new TimerTask() {
-            @Override
-            public void run() {
-                ServerReceptionCoordinates src = new ServerReceptionCoordinates(game);
-                src.execute(String.valueOf(game.getId()));
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        afficherJoueurs();
-                    }
-                });
-
-                ServerSendCoordinates ssc = new ServerSendCoordinates();
-
-                ssc.execute(pseudo, String.valueOf(game.getId()), String.valueOf(location.getLatitude()),  String.valueOf(location.getLongitude()));
-            }
-        };
-        timer.schedule(timerTask, 0, 1000 * 2);
-
-    }
-
-    public void launchServerRequest(Game game){
-        Log.i("Début", "requete serveur flags");
-        ServerReceptionFlagsPositions srf = new ServerReceptionFlagsPositions(game, this);
-        srf.execute();
-    }
-
-    public void initialisationObjetsLocalises(Game game){
-        Log.i("Init", "Initialisation flags");
-
-        for (Flag flag : game.getBlueTeam().getListofFlags()) {
-            mMap.addMarker(new MarkerOptions().position(flag.getCoordonnees()).title(flag.getName()));
-        }
-
-        for (Flag flag : game.getRedTeam().getListofFlags()) {
-            mMap.addMarker(new MarkerOptions().position(flag.getCoordonnees()).title(flag.getName()));
-        }
-    }
-
-    public void demanderPermissionGps(){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+    //Méthodes en rapport avec la demande d'accès au GPS
+    public void demanderPermissionGps() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setMyLocationButtonEnabled(false);
             mMap.getUiSettings().setCompassEnabled(false);
         } else {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE);
         }
 
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         Criteria criteria = new Criteria();
         String bestProvider = locationManager.getBestProvider(criteria, true);
-        location = locationManager.getLastKnownLocation(bestProvider);
-        if (location != null) {
-            onLocationChanged(location);
+        if (bestProvider == null) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {}
+                        @Override
+                        public void onProviderEnabled(String provider) {}
+                        @Override
+                        public void onProviderDisabled(String provider) {}
+                        @Override
+                        public void onLocationChanged(final Location location) {}
+                    }
+            );
         }
+        location = locationManager.getLastKnownLocation(bestProvider);
+        /*if (location != null) {
+            onLocationChanged(location);
+        }*/
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 1, this);
-        Log.i("Location", location.getLatitude()+" "+location.getLongitude());
     }
 
-
-
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case REQUEST_CODE: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                         mMap.setMyLocationEnabled(true);
                     }
-
                 } else {
                     demanderPermissionGps();
                 }
-                return;
+                break;
             }
-
         }
     }
-
-    public void retirerMouvementCameraMarkers(){
-        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-
-            Marker currentShown;
-
-            public boolean onMarkerClick(Marker marker) {
-                if (marker.equals(currentShown)) {
-                    marker.hideInfoWindow();
-                    currentShown = null;
-                } else {
-                    marker.showInfoWindow();
-                    currentShown = marker;
-                }
-                return true;
-            }
-        });
-    }
-
 
     public static void setDefencetoken(boolean defenceTokenAvailable){
         if(defenceTokenAvailable){
@@ -364,7 +374,6 @@ public class EcranJeu extends FragmentActivity implements OnMapReadyCallback, Lo
         else{
             defenceToken.setImageResource(R.drawable.jeton_blanc_et_tour);
         }
-
     }
 
     public static void setAttackToken(boolean attackTokenAvailable){
@@ -374,13 +383,6 @@ public class EcranJeu extends FragmentActivity implements OnMapReadyCallback, Lo
         else{
             attackToken.setImageResource(R.drawable.jeton_noir_et_tour);
         }
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        this.location = location;
-        Log.i("Location", location.getLatitude()+" "+location.getLongitude());
     }
 
     @Override
