6e52ed5a5977e02a42e89143b5d5bce720f83cc1
Assassange <mattlb53@gmail.com>
2017-01-05 22:17:54 +0100

    verif caractères

--- app/src/main/java/com/example/florian/altarconquest/View/EcranCreation_Partie.java
+++ app/src/main/java/com/example/florian/altarconquest/View/EcranCreation_Partie.java
@@ -3,7 +3,7 @@ package com.example.florian.altarconquest.View;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,6 +22,7 @@ public class EcranCreation_Partie extends Activity implements AdapterView.OnItem
     EditText nomPartie;
     EditText passwordPartie;
     Game game;
+    char charList[] = {'-', '\"', '~', '\'', '_', '(', ')', '@', '[', ']', '{', '}', '#'};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,30 +40,100 @@ public class EcranCreation_Partie extends Activity implements AdapterView.OnItem
 
         ImageButton bouton_retour = (ImageButton) findViewById(R.id.bouton_retour);
         bouton_retour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                ouvrirGestion_Partie();
-            }}
+                                             @Override
+                                             public void onClick(View v) {
+                                                 ouvrirGestion_Partie();
+                                             }
+                                         }
         );
 
         ImageButton bouton_creer_la_partie = (ImageButton) findViewById(R.id.bouton_creer_la_partie);
         bouton_creer_la_partie.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                Log.i("Création partie", "");
-                //Insertion game dans la bdd
-                ServerSendGameProperties ssgp = new ServerSendGameProperties();
-                ssgp.execute(addQuote(nomPartie.getText().toString()), addQuote(passwordPartie.getText().toString()), addQuote(String.valueOf(spinner.getSelectedItem())));
+                                                      @Override
+                                                      public void onClick(View v) {
+                                                          //Appel de la fonction vérifiant que les entrées ne posent pas de problème
+                                                          verification();
 
-                ouvrirRejoindrePartie();
-            }}
+                                                      }
+                                                  }
         );
     }
 
-    public String addQuote(String chaine){
-        return "'"+chaine+"'";
+    public String addQuote(String chaine) {
+        return "'" + chaine + "'";
+    }
+
+    /**
+     * @param name
+     * @return false si le String contient un caractère interdit
+     */
+    private boolean isNameValid(String name) {
+
+        for (int i = 0; i < charList.length; i++)
+            if (name.contains(Character.toString(charList[i])))
+                return false;
+        return true;
+
+    }
+
+    /**
+     * @param password
+     * @return false si le String contient un caractère interdit
+     */
+    private boolean isPasswordValid(String password) {
+
+        for (int i = 0; i < charList.length; i++) {
+            if (password.contains(Character.toString(charList[i])))
+                return false;
+        }
+        return true;
+
+    }
+
+    /**
+     * fonction vérifiant les entrées et permettant ou non la création de la partie
+     */
+    private void verification() {
+
+        // Reset errors
+        nomPartie.setError(null);
+        passwordPartie.setError(null);
+
+        //enregistre les Strings venant des textEdits
+        String name = nomPartie.getText().toString();
+        String password = passwordPartie.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            passwordPartie.setError("Caractères interdits dans le mot de passe");
+            focusView = passwordPartie;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(name)) {
+            nomPartie.setError("Ce champs est obligatoire");
+            focusView = nomPartie;
+            cancel = true;
+        } else if (!isNameValid(name)) {
+            nomPartie.setError("Caractères interdits dans le nom");
+            focusView = nomPartie;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // Focus sur le champs posant un problème
+            focusView.requestFocus();
+        } else {
+
+            //Insertion game dans la bdd
+            ServerSendGameProperties ssgp = new ServerSendGameProperties();
+            ssgp.execute(addQuote(nomPartie.getText().toString()), addQuote(passwordPartie.getText().toString()), addQuote(String.valueOf(spinner.getSelectedItem())));
+
+            ouvrirRejoindrePartie();
+
+        }
     }
 
     public void ouvrirGestion_Partie() {
@@ -71,7 +142,6 @@ public class EcranCreation_Partie extends Activity implements AdapterView.OnItem
     }
 
     public void ouvrirRejoindrePartie() {
-        Log.i("Choix equipe", "");
         Intent intent = new Intent(this, EcranRejoindre_Partie.class);
         startActivity(intent);
     }
